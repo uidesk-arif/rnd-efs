be19f59bce0dac47d1ae8b55da4d4695b3d1e415
Rahmad Arif <rahmadarif28080@gmail.com>
2023-01-03 20:47:15 +0700

    add wb

--- api/btpnDemoWidgetInterestIndicator.php
+++ api/btpnDemoWidgetInterestIndicator.php
@@ -1,5 +1,5 @@
 <?php
-$query = $koneksi->query("SELECT (SELECT COUNT(OutboundListEFS.JidON)  FROM OutboundListEFS OutboundListEFS WHERE OutboundListEFS.StatusCall='new') as interest, (SELECT COUNT(OutboundListEFS_2.JidON) FROM OutboundListEFS OutboundListEFS_2)  as total");
+$query = $koneksi->query("SELECT (SELECT COUNT(OutboundListEFS.JidON)  FROM OutboundListEFS OutboundListEFS WHERE OutboundListEFS.StatusCall='interest') as interest, (SELECT COUNT(OutboundListEFS_2.JidON) FROM OutboundListEFS OutboundListEFS_2)  as total");
 $datas = $query->fetch_assoc();
 
 $result['status'] = true;
