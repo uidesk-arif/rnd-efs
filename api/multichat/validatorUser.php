c18a7e9bbeb70c8284cb93fa1f50bedb4089e90b
Rahmad Arif <rahmadarif28080@gmail.com>
2023-01-09 14:42:28 +0700

    fixing multichat validator

--- api/multichat/validatorUser.php
+++ api/multichat/validatorUser.php
@@ -2,15 +2,16 @@
 if(!empty(file_get_contents("php://input"))) {
     $_post = json_decode(file_get_contents("php://input"), true);
 
-    if($_post['user']['account_id'] == "admin" && $_post['userid'] == "6282285768836") {
+    if($_post['message'] == "admin" && $_post['user']['account_id'] == "6282285768836") {
         $result['status'] = true;
         $result['msg'] = "authentication success as admin";
-    } else if($_post['user']['account_id'] == "user" && $_post['userid'] == 2) {
+    } else if($_post['message'] == "user" && $_post['user']['account_id'] == "6281277409714") {
         $result['status'] = true;
         $result['msg'] = "authentication success as user";
     } else {
         $result['status'] = false;
-        $result['msg'] = "authentication failed (2)";
+        // $result['msg'] = "authentication failed (2);".json_encode($_post);
+        $result['msg'] = "authentication failed (2);";
     }
 } else {
     $result['status'] = false;
