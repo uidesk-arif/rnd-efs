07b3e1e462c060d6c173d7a813b3f20e11877773
Rahmad Arif <rahmadarif28080@gmail.com>
2023-06-05 11:45:08 +0700

    add connection

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,5 @@
 <?php
 
-$koneksi = new mysqli("localhost", "root", "", "rnd_efs");
+// $koneksi = new mysqli("localhost", "root", "", "rnd_efs");
+$koneksi = new mysqli("localhost", "u1495171_rnd", "s8O*qJpw0EvF", "u1495171_pds");
 if($koneksi->connect_errno) die($koneksi -> connect_error);
