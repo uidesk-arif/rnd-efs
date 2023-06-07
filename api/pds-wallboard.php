40aa204fe642a4e5cb48677dd7c8d507f8964d94
Rahmad Arif <rahmadarif28080@gmail.com>
2023-06-07 07:49:13 +0700

    fix avg call

--- api/pds-wallboard.php
+++ api/pds-wallboard.php
@@ -573,7 +573,7 @@ $campaign_detail_response = json_decode($campaign_detail_response, true);
                                     <center>
                                         <div class="box">
                                             <div class="box-body">
-                                                <h5 class="box-title text-warning">Avg Call</h5>
+                                                <h5 class="box-title text-warning">Average Call Duration</h5>
                                                 <h1 class="text-warning" id="DataCompleteCall"><?= date("H:i:s", floor($campaign_detail_response['stats']['update']['total_sec']/ $campaign_detail_response['statuscount']['update']['finished'])) ?></h1>
                                             </div>
                                         </div>
